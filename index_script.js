7e7ac805e7511eb632d0cf485a6c25a5ee8e72a5
Julian Müller <juli.m18@gmx.de>
2023-04-16 03:01:10 +0200

    v3

--- index_script.js
+++ index_script.js
@@ -161,7 +161,7 @@ function generateContent() {
     }
 
     content = content.sort(cmp);
-    document.getElementById("accommodations-div").innerHTML = content.map(value => value[1]).join('');
+    document.getElementById("accommodations-div").innerHTML = content.join('');//.map(value => value[1])
 
     const all = document.getElementsByClassName("accommodation-container");
     for (let e of all) {
