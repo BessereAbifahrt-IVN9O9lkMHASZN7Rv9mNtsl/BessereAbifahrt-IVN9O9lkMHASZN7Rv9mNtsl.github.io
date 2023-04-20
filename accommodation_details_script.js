05d294e2653cba2991a55673bce6e5b6579cf29f
Julian Müller <juli.m18@gmx.de>
2023-04-20 10:41:48 +0200

    v439

--- accommodation_details_script.js
+++ accommodation_details_script.js
@@ -13,3 +13,38 @@ for (let i = 0; i < imgs.length; i++) {
     });
 }
 
+let time;
+let moved = false;
+let startX = 0;
+let startY = 0;
+let deltaX = 0;
+let deltaY = 0;
+main.addEventListener("touchstart", evt => {
+    time = evt.timeStamp;
+    const obj = evt.changedTouches[0];
+    startX = obj.clientX;
+    startY = obj.clientY;
+    moved = false;
+});
+main.addEventListener("touchmove", evt => {
+    const obj = evt.changedTouches[0];
+    deltaX = obj.clientX - startX;
+    deltaY = obj.clientY - startY;
+    if (Math.abs(deltaX) > Math.abs(deltaY) || moved) {
+        evt.preventDefault();
+        moved = true;
+        main.style.left = deltaX + 'px';
+    }
+});
+main.addEventListener("touchend", evt => {
+    evt.preventDefault();
+    main.style.left = '0';
+    if (moved && deltaX < -100 && current_image < imgs.length - 1) {
+        current_image++;
+        main.src = imgs[current_image].src;
+    }
+    if (moved && deltaX > 100 && current_image > 0) {
+        current_image--;
+        main.src = imgs[current_image].src;
+    }
+});
