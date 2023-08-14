caa048dd60c69261673f8a1cd0ddbccf76af39a8
Julian Müller <juli.m18@gmx.de>
2023-08-14 23:12:29 +0200

    v611

---
+++ accommodation_details_script_v611.js
@@ -0,0 +1,68 @@
+const thumbnails = document.getElementById("other-images");
+const imgs = thumbnails.getElementsByTagName("img");
+const main = document.getElementById("main-image");
+
+let current_image = -1;
+
+for (let i = 0; i < imgs.length; i++) {
+    let img = imgs[i];
+
+    img.addEventListener("click", function () {
+        set_main_image(i);
+    });
+}
+
+set_main_image(0);
+
+let time;
+let moved = false;
+let startX = 0;
+let startY = 0;
+let deltaX = 0;
+let deltaY = 0;
+main.addEventListener("touchstart", evt => {
+    time = evt.timeStamp;
+    const obj = evt.changedTouches[0];
+    startX = obj.clientX;
+    startY = obj.clientY;
+    moved = false;
+});
+main.addEventListener("touchmove", evt => {
+    const obj = evt.changedTouches[0];
+    deltaX = obj.clientX - startX;
+    deltaY = obj.clientY - startY;
+    if (Math.abs(deltaX) > Math.abs(deltaY) || moved) {
+        evt.preventDefault();
+        moved = true;
+        let max = 10;
+        let min = -10;
+        if (current_image <= 0)
+            max = 0;
+        else if (current_image >= imgs.length - 1)
+            min = 0;
+        if (deltaX > max + 2)
+            startX = obj.clientX - max + 1;
+        if (deltaX < min - 2)
+            startX = obj.clientX - min - 1;
+        main.style.left = Math.max(min, Math.min(deltaX, max)) + 'px';
+    }
+});
+main.addEventListener("touchend", evt => {
+    evt.preventDefault();
+    main.style.left = '0';
+    if (moved && deltaX <= -8 && current_image < imgs.length - 1) {
+        set_main_image(current_image + 1);
+    }
+    if (moved && deltaX >= 8 && current_image > 0) {
+        set_main_image(current_image - 1);
+    }
+});
+
+function set_main_image(i) {
+    main.src = imgs[i].src;
+    imgs[i].className += ' selected';
+    if (current_image >= 0) {
+        imgs[current_image].className = imgs[current_image].className.replaceAll('selected', '');
+    }
+    current_image = i;
+}
---
+++ index_script_v611.js
@@ -0,0 +1,372 @@
+// Disable Js warning
+document.getElementById("content").classList.remove("hidden");
+document.getElementById("java-script-disabled-warning").classList.add("hidden");
+
+let accommodations = [{
+  "Name": "Seepark Auenhain",
+  "Description": "Die Unterkunft liegt sehr nahe am See und Strand und bietet Wassersport Möglichkeiten. Die Unterkunft ist aber in einer Siedlung gelegen und nur knapp stündlich mit einem Bus erreichbar. Einkaufsmöglichkeiten, sowie Möglichkeiten zum Essen gibt es in der Nähe nicht. Die Unterkunft besteht aus mehreren Häusern und bei unserer Gruppengröße müssten wir 2 Häuser nehmen. Daher ist die Bettenverteilung eine Zusammenfassung beider Häuser.",
+  "SurroundingTags": [
+    "See",
+    "Siedlung"
+  ],
+  "Country": "Deutschland",
+  "Region": "Sachsen",
+  "AdditionalLocationInformation": "in der Nähe von Leipzig",
+  "Address": "Am Feriendorf 2, 04416 Markkleeberg, Deutschland",
+  "TravelTime": 2.5,
+  "TravelDescription": "Mit dem ICE bis Leipzig (1.5h) und von dort mit der S Bahn und einem Bus (je ca 15 min).",
+  "MaxPersonCount": 10,
+  "Rooms": [
+    {
+      "SingleBeds": 2,
+      "DoubleBeds": 0,
+      "SofaBeds": 0
+    },
+    {
+      "SingleBeds": 2,
+      "DoubleBeds": 0,
+      "SofaBeds": 0
+    },
+    {
+      "SingleBeds": 0,
+      "DoubleBeds": 1,
+      "SofaBeds": 0
+    },
+    {
+      "SingleBeds": 0,
+      "DoubleBeds": 1,
+      "SofaBeds": 0
+    },
+    {
+      "SingleBeds": 0,
+      "DoubleBeds": 1,
+      "SofaBeds": 0
+    },
+    {
+      "SingleBeds": 0,
+      "DoubleBeds": 0,
+      "SofaBeds": 1
+    },
+    {
+      "SingleBeds": 0,
+      "DoubleBeds": 0,
+      "SofaBeds": 1
+    }
+  ],
+  "TimeSpan": "08.07. - 14.07.2024",
+  "TotalPrice": 2678,
+  "SmallImageLink": "https://cf.bstatic.com/xdata/images/hotel/square200/74488497.webp?k=6a9f4721e2407be9a6ec8fc0ee1d4e51a8aabd24736d4ac975314c1a5fcb0543&o=",
+  "Images": [
+    "https://cf.bstatic.com/images/hotel/max1024x768/189/189423865.jpg",
+    "https://cf.bstatic.com/images/hotel/max1024x768/411/41152939.jpg",
+    "https://cf.bstatic.com/images/hotel/max1024x768/411/41152946.jpg",
+    "https://cf.bstatic.com/images/hotel/max1024x768/411/41152945.jpg",
+    "https://cf.bstatic.com/images/hotel/max1024x768/910/91098143.jpg",
+    "https://cf.bstatic.com/images/hotel/max1024x768/411/41152950.jpg",
+    "https://cf.bstatic.com/images/hotel/max1024x768/139/139543383.jpg",
+    "https://cf.bstatic.com/images/hotel/max1024x768/911/91128572.jpg"
+  ],
+  "ProArguments": [
+    "Bieten Frühstück an, was sehr gut sein soll",
+    "Insgesamt 4 Badezimmer",
+    "Nahe am Strand",
+    "Wassersport Möglichkeiten"
+  ],
+  "ContraArguments": [
+    "Unterbringung in 2 voneinander getrenten Häusern",
+    "Sehr abgelegen",
+    "Keine Einkaufs-/Essmöglichkeiten in der Gegend",
+    "Anbindung nur mit einem stündlich fahrenden Bus"
+  ],
+  "InformationDate": "14.08.2023",  
+  "BookingComLink": "https://www.booking.com/Share-a2eL9Ox"
+},
+{
+  "Name": "Parkside Boutique Appartements",
+  "Description": "Die Unterkunft liegt im historischen Teil von Herbolzheim und in der Nähe des Marktplatzes. Herbolzheim ist eine Kleinstadt, die aber eine ausreichende Anbindung mit dem Regio hat. In der Nähe befindet sich unter anderem der Europapark, der mit einem Bus in ca. 30 min zu erreichen ist und eine größere Golfanlage.",
+  "SurroundingTags": [
+    "Kleinstadt"
+  ],
+  "Country": "Deutschland",
+  "Region": "Baden-Württemberg",
+  "AdditionalLocationInformation": "",
+  "Address": "Hauptstraße 41, 79336 Herbolzheim, Deutschland",
+  "TravelTime": 7,
+  "TravelDescription": "Mit dem ICE nach Baden-Baden (6h) und von dort mit einem Regio (1h).",
+  "MaxPersonCount": 10,
+  "Rooms": [
+    {
+      "SingleBeds": 0,
+      "DoubleBeds": 1,
+      "SofaBeds": 0
+    },
+    {
+      "SingleBeds": 0,
+      "DoubleBeds": 1,
+      "SofaBeds": 0
+    },
+    {
+      "SingleBeds": 0,
+      "DoubleBeds": 1,
+      "SofaBeds": 0
+    },
+    {
+      "SingleBeds": 0,
+      "DoubleBeds": 1,
+      "SofaBeds": 0
+    },
+    {
+      "SingleBeds": 0,
+      "DoubleBeds": 0,
+      "SofaBeds": 1
+    }
+  ],
+  "TimeSpan": "07.07. - 13.07.2024",
+  "TotalPrice": 2084,
+  "SmallImageLink": "https://cf.bstatic.com/xdata/images/hotel/square200/153806432.webp?k=6b5d84aad8e9928dbc385c6139c1287c94343c97ef5c5a5553c70a72a058fa58&o=",
+  "Images": [
+    "https://cf.bstatic.com/images/hotel/max1024x768/155/155476297.jpg",
+    "https://cf.bstatic.com/images/hotel/max1024x768/155/155476292.jpg",
+    "https://cf.bstatic.com/images/hotel/max1024x768/155/155476281.jpg",
+    "https://cf.bstatic.com/images/hotel/max1024x768/155/155472543.jpg"
+  ],
+  "ProArguments": [
+    "In der Nähe des Europaparks (Pro, wenn man da hin möchte)",
+    "Es ist ein zusammenhängendes Apartment (es gibt daher nicht mehrere komplett voneinander getrente Zimmer)"
+  ],
+  "ContraArguments": [
+    "Nur ein Bad",
+    "Relativ abgelegen und in der Umgebung überwiegend Felder"
+  ],
+  "InformationDate": "14.08.2023",
+  "BookingComLink": "https://www.booking.com/Share-XR3MIQ"
+},
+];
+
+const preset = "<div class=\"accommodation-container\">     <img class=\"accommodation-image\" src=\"SmallImageLink\">     <div class=\"accommodation-sub-container\">         <div class=\"accommodation-info-div\">             <p class=\"accommodation-name\">Name</p>             <p class=\"accommodation-location\">Country, Region AdditionalLocationInformation</p>             <div class=\"accommodation-surrounding-tags\">%Tags%</div>             <p class=\"accommodation-capacity\">MaxPersonCount Personen</p>         </div>         <div class=\"accommodation-new-label-div\">             <p class=\"accommodation-new-label\">NEU</p>         </div>         <table class=\"accommodation-pricing-table\">             <tr>                 <td class=\"accommodation-pricingInfo\">Total:</td>                 <td class=\"accommodation-totalPrice\">TotalPrice€</td>             </tr>             <tr>                 <td class=\"accommodation-pricingInfo\">Pro Person (bei MaxPersonCount Pers.):</td>                 <td class=\"accommodation-perPersonPricing\">PerPersonPrice€</td>             </tr>         </table>     </div> </div>";
+
+const tag_colors = {
+  "Wald": "#55FF55",
+  "Fluss": "#5555FF",
+  "See": "#999999",
+  "Kleinstadt": "#999999",
+  "Siedlung": "#999999"
+};
+
+let selected_countries = [];
+let selected_regions = [];
+let selected_surrounding_tags = [];
+let filter_new = false;
+
+accommodations = accommodations.sort((a, b) => a['Name'].localeCompare(b['Name']));
+
+generateContent();
+
+const country_filter_options = document.getElementById("country-filter").children;
+for (let option of country_filter_options) {
+    option.addEventListener("click", function () {
+        if (option.className.includes('selected')) {
+            option.className = option.className.replaceAll('selected', '');
+            selected_countries.splice(selected_countries.indexOf(option.innerHTML), 1);
+        } else {
+            option.className += ' selected';
+            selected_countries.push(option.innerHTML);
+        }
+        updateRegionSelection();
+    });
+}
+
+const region_filter_options = document.getElementById('region-filter').children;
+for (let regionFilterOption of region_filter_options) {
+    regionFilterOption.addEventListener("click", function () {
+        if (regionFilterOption.className.includes('selected')) {
+            regionFilterOption.className = regionFilterOption.className.replaceAll('selected', '');
+            selected_regions.splice(selected_regions.indexOf(regionFilterOption.innerHTML), 1);
+        } else {
+            regionFilterOption.className += ' selected';
+            selected_regions.push(regionFilterOption.innerHTML);
+        }
+
+        generateContent();
+    });
+}
+
+const surrounding_filter_options = document.getElementById('surrounding-selection').children;
+for (let surroundingFilterOption of surrounding_filter_options) {
+    surroundingFilterOption.addEventListener('click', () => {
+        if (surroundingFilterOption.className.includes('selected')) {
+            surroundingFilterOption.className = surroundingFilterOption.className.replaceAll('selected', '');
+            selected_surrounding_tags.splice(selected_surrounding_tags.indexOf(surroundingFilterOption.innerHTML), 1);
+        } else {
+            surroundingFilterOption.className += ' selected';
+            selected_surrounding_tags.push(surroundingFilterOption.innerHTML);
+        }
+
+        generateContent();
+    });
+}
+
+const new_filter = document.getElementById('new-filter');
+new_filter.addEventListener("click", function () {
+    if (new_filter.className.includes('selected')) {
+        new_filter.className = new_filter.className.replaceAll('selected', '');
+        filter_new = false;
+    } else {
+        new_filter.className += ' selected';
+        filter_new = true;
+    }
+
+    generateContent();
+});
+
+function updateRegionSelection() {
+    for (let regionFilterOption of region_filter_options) {
+        if (selected_countries.length === 0 || selected_countries.some(value => regionFilterOption.className.includes(value)))
+            regionFilterOption.style.display = 'block';
+        else {
+            regionFilterOption.style.display = 'none';
+            if (regionFilterOption.className.includes('selected')) {
+                regionFilterOption.className = regionFilterOption.className.replaceAll('selected', '');
+                selected_regions.splice(selected_regions.indexOf(regionFilterOption.innerHTML), 1);
+            }
+        }
+    }
+
+    generateContent();
+}
+
+
+function generateContent() {
+    let content = [];
+
+    for (let data of accommodations) {
+
+        if (selected_countries.length > 0 && !selected_countries.includes(data['Country'])
+            || selected_regions.length > 0 && !selected_regions.includes(data['Region'])
+            || selected_surrounding_tags.length > 0 && !selected_surrounding_tags.every(e => data['SurroundingTags'].includes(e)))
+            continue;
+
+        const accommodations_seen = getCookie('AccommodationsSeen');
+        const seen = accommodations_seen.split(',').includes(SafeName(data['Name']));
+
+        if (filter_new && seen) continue;
+
+        let presetCopy = preset;
+        for (let key in data) {
+            presetCopy = presetCopy.replaceAll(key, data[key]);
+        }
+        if (seen)
+            presetCopy = presetCopy.replaceAll('<div class="accommodation-new-label-div">',
+                '<div class="accommodation-new-label-div" style="display: none">');
+
+        let tags = '';
+        for (let i in data['SurroundingTags']) {
+            let tag = data['SurroundingTags'][i];
+            tags += `<div style="background-color: ${tag_colors[tag]}">${tag}</div>`;
+        }
+
+        presetCopy = presetCopy.replaceAll('%Tags%', tags);
+
+        content.push([seen, presetCopy.replaceAll("PerPersonPrice", Math.round(data["TotalPrice"] / data["MaxPersonCount"]).toString())]);
+    }
+
+    function cmp(a, b) {
+        if (a[0] && !b[0])
+            return 1;
+        if (!a[0] && b[0])
+            return -1;
+        else return 0;
+    }
+
+    content = content.sort(cmp);
+    document.getElementById("accommodations-div").innerHTML = content.map(value => value[1]).join('');
+
+    const all = document.getElementsByClassName("accommodation-container");
+    // swipe
+    for (let e of all) {
+        const click = () => {
+            const safe_name = SafeName(e.getElementsByClassName("accommodation-name")[0].innerHTML);
+
+            const accommodations_seen = getCookie('AccommodationsSeen');
+            if (!accommodations_seen.split(',').includes(safe_name)) {
+                let expiration_date = new Date();
+                expiration_date.setFullYear(expiration_date.getFullYear() + 5);
+                if (accommodations_seen === '')
+                    document.cookie = "AccommodationsSeen=" + safe_name + '; path=/; expires=' + expiration_date.toUTCString();
+                else
+                    document.cookie = "AccommodationsSeen=" + accommodations_seen + ',' + safe_name + '; path=/; expires=' + expiration_date.toUTCString();
+                if (filter_new)
+                    generateContent();
+            }
+
+            e.getElementsByClassName("accommodation-new-label-div")[0].style.display = 'none';
+            window.open(safe_name + '.html', '_blank').focus()
+        };
+        e.addEventListener("click", click);
+        let time;
+        let movedX = false;
+        let movedY = false;
+        let startX = 0;
+        let startY = 0;
+        let deltaX = 0;
+        let deltaY = 0;
+        e.addEventListener("touchstart", evt => {
+            time = evt.timeStamp;
+            const obj = evt.changedTouches[0];
+            startX = parseInt(obj.clientX);
+            startY = parseInt(obj.clientY);
+            movedX = false;
+            movedY = false;
+        });
+        e.addEventListener("touchmove", evt => {
+            const obj = evt.changedTouches[0];
+            deltaX = parseInt(obj.clientX) - startX;
+            deltaY = parseInt(obj.clientY) - startY;
+            if (Math.abs(deltaX) > Math.abs(deltaY) || movedX) {
+                evt.preventDefault();
+                movedX = true;
+                if (deltaX > 2)
+                    startX = obj.clientX - 1;
+                e.style.left = Math.min(deltaX, 0) + 'px';
+            } else if (Math.abs(deltaY) > 100) {
+                movedY = true;
+            }
+        });
+        e.addEventListener("touchend", evt => {
+            evt.preventDefault();
+            if (evt.timeStamp - time <= 500 && !movedX && !movedY)
+                click();
+            else {
+                e.style.left = '0';
+                if (movedX && deltaX < -100) {
+                    click();
+                }
+            }
+        });
+    }
+}
+
+function SafeName(name) {
+    return name
+        .replaceAll(' ', '_')
+        .replaceAll('ö', 'oe')
+        .replaceAll('ä', 'ae')
+        .replaceAll('ü', 'ue')
+        .replaceAll('ß', 'ss');
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
